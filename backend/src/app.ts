f2e051785959a1947a5570ff6dc53ac3d7d81297
Tony Ni <tonyni@Tonys-MacBook-Pro.local>
2023-01-26 17:55:56 -0500

    setup eslint and prettier config files for easy linting and formatting of code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hello world")
-})
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
-app.listen(8080, () => {console.log("server running")})
+app.listen(8080, () => {
+  console.log('server running');
+});
