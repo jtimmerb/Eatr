33ee96a5cd673b61699c96aa58f1907ed9e3c54c
Javon <jtimmerb@purdue.edu>
2023-01-29 16:06:17 -0500

    Added connection to AWS mySQL DB in app.ts. Configurations held in db.config.json (private)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,23 @@
 import express from 'express';
 
+const {db_host, db_user, db_pwd, db_name, db_port} = require('./db.config.json');
+var mysql = require('mysql');
 const app = express();
+
+var db_connection = mysql.createConnection({
+  host: db_host,
+  user: db_user,
+  password: db_pwd,
+  port: db_port,
+});
+
+db_connection.connect(function (err: {stack: string}) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world');
