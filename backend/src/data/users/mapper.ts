39e0a2c664c8b5378e295b84165ae66d7dd1a06a
Javon <jtimmerb@purdue.edu>
2023-02-06 18:04:33 -0500

    Updated comments in mapper.ts

--- backend/src/data/users/mapper.ts
+++ backend/src/data/users/mapper.ts
@@ -2,9 +2,9 @@ import {User} from './entity';
 import {Mapper} from '..';
 import {int} from 'aws-sdk/clients/datapipeline';
 
-/** Mapper method that maps Player Entity to Player DB entity and vice versa */
+/** Mapper method that maps User Entity to User DB entity and vice versa */
 export const UserMapper: Mapper<User> = class {
-  /** Mapping from Player DB Entity to Player Entity  */
+  /** Mapping from User DB Entity to User Entity  */
   public static fromDB(d: string): User {
     let data = JSON.parse(JSON.stringify(d));
     return {
