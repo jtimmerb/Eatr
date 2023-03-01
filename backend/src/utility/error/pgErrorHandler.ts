987c247c28b2a88baf8a4b5d5140fa512ee6ab9b
Javon <jtimmerb@purdue.edu>
2023-02-28 23:08:28 -0500

    Add basic error codes

--- backend/src/utility/error/pgErrorHandler.ts
+++ backend/src/utility/error/pgErrorHandler.ts
@@ -1,6 +1,14 @@
 import {DatabaseError} from 'pg';
 export default class PgErrorHandler {
   public handleError(err: DatabaseError) {
-    console.log(err.code);
+    if (err.code === '01000') {
+      console.log('Warning');
+    } else if (err.code === '02000') {
+      console.log('No Data');
+    } else if (err.code === '03000') {
+      console.log('SQL Incomplete');
+    } else if (err.code === '08000') {
+      console.log('Connection Exception');
+    }
   }
 }
