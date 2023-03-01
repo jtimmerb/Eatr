0cd6286bf2140a19cc51c5820b014486837cf841
Varun Dengi <vdengi@purdue.edu>
2023-02-28 20:08:43 -0500

    implemented recipeingredient-controller

--- backend/src/biz/recipe-controller/recipe-controller.ts
+++ backend/src/biz/recipe-controller/recipe-controller.ts
@@ -4,12 +4,25 @@ import RepoController from '../repoController';
 
 export default class RecipeController extends RepoController<RecipeRepo> {
   public createRecipe = async (newRecipe: Recipe): Promise<Recipe> => {
-    const Recipe = await this.getRepo().create(newRecipe);
-
-    return Recipe;
+    const recipe = await this.getRepo().create(newRecipe);
+    return recipe;
   };
 
   public deleteRecipe = async (recipe: Recipe): Promise<void> => {
     const res = await this.getRepo().delete(recipe);
+  };
+
+  public updateRecipe = async (newRecipe: Recipe): Promise<void> => {
+    const res = await this.getRepo().update(newRecipe);
+  };
+
+  public getRecipe = async (recipeID: number): Promise<Recipe> => {
+    const recipe: Recipe = {
+      recipeId: recipeID,
+      name : "",
+      steps: []
+  }
+    const recipeReceived = await this.getRepo().get(recipe);
+    return recipeReceived
   };
 }
