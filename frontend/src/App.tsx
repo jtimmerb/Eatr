4a6de82f3074a81d35ec8f885b7edfdcbc8fd644
Javon <jtimmerb@purdue.edu>
2023-04-04 10:28:32 -0400

    boilerplate for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <p className="text-xl text-red-400">I LOVE MEN</p>
+    </div>
+  );
+}
+
+export default App;
