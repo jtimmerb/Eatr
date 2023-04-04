a3f0b2009ea3245aceca465ebf0a648127e98fdd
Tony Ni <31938397+TonyNiTN@users.noreply.github.com>
2023-04-04 10:30:59 -0400

    Update App.tsx
    update initial code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <p className="text-xl text-red-400">Eatr Base</p>
+    </div>
+  );
+}
+
+export default App;
