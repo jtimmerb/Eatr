16ee2676cc8fbd111d2eb244bab21bd6757583b6
VarunDengi <vdengi@purdue.edu>
2023-04-13 15:30:08 -0900

    added tests for users and ingredients

---
+++ backend/src/testObjects/testObjects.ts
@@ -0,0 +1,45 @@
+import {User} from '../data/users/entity';
+import { UserRecipe } from '../data/user-recipe/entity';
+import { UserPantry } from '../data/user-pantry/entity';
+import { RecipeIngredient } from '../data/recipe-ingredient/entity';
+import {Recipe} from '../data/recipes/entity';
+import { Ingredient } from '../data/ingredient/entity';
+
+export const userOne= {
+    name : 'userOne'
+}
+
+export const userTwo= {
+    name : 'userTwo'
+}
+
+export const userThree= {
+    name : 'userThree'
+}
+
+export const ingredientOnion= {
+  name: 'onion',
+  servingSize: '1/3 cup',
+  calories: 1,
+  proteins: 2,
+  carbohydrates: 3,
+  fats: 4
+}
+
+export const ingredientTomato= {
+  name: 'tomato',
+  servingSize: '1/3 cup',
+  calories: 1,
+  proteins: 2,
+  carbohydrates: 3,
+  fats: 4
+}
+
+export const ingredientCucumber= {
+  name: 'cucumber',
+  servingSize: '1/3 cup',
+  calories: 1,
+  proteins: 2,
+  carbohydrates: 3,
+  fats: 4
+}  
