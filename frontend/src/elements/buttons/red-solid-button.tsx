8e5c7f3329e7f939471a524eb5c9384a8f6fec45
Tony Ni <tony.zt.ni@gmail.com>
2023-04-24 18:24:55 -0400

    change login page to use predefined buttons

--- frontend/src/elements/buttons/red-solid-button.tsx
+++ frontend/src/elements/buttons/red-solid-button.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const RedSolidButton: React.FC<IProps> = ({ onClick, children, className }) => {
   return (
     <button
-      className={`w-1/2 py-2 px-4 border border-transparent text-sm font-medium rounded-2xl text-white bg-red-400 ${className}`}
+      className={`w-52 py-2 px-4 border border-transparent text-sm font-medium rounded-2xl text-white bg-red-400 shadow-md ${className}`}
       onClick={onClick}
     >
       {children}
--- frontend/src/elements/buttons/white-solid-button.tsx
+++ frontend/src/elements/buttons/white-solid-button.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const WhiteSolidButton: React.FC<IProps> = ({ onClick, children, className }) => {
   return (
     <button
-      className={`w-1/2 py-2 px-4 border border-transparent text-sm font-medium rounded-2xl text-red-400 bg-white ${className}`}
+      className={`w-52 py-2 px-4 border border-transparent text-sm font-medium rounded-2xl text-red-400 bg-white shadow-md ${className}`}
       onClick={onClick}
     >
       {children}
