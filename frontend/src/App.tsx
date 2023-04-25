399246f6b26392a2b902e8642f8ef455f4564185
Tony Ni <tony.zt.ni@gmail.com>
2023-04-25 09:20:06 -0400

    fix merge conflict for app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,38 @@ import LandingPage from "./pages/landing-page";
 import ErrorPage from "./pages/error-page";
 import LoginPage from "./pages/loginPage";
 import SignUpPage from "./pages/signupPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import React from "react";
-
-import LandingPage from "./pages/landing-page";
-import ErrorPage from "./pages/error-page";
-import LoginPage from "./pages/loginPage";
-import SignUpPage from "./pages/signupPage";
 import UserHome from "./pages/userHomePage";
 import SavedRecipesPage from "./pages/savedRecipesPage";
 import UserPantryPage from "./pages/userPantryPage";
 import DiscoverPage from "./pages/discoverPage";
-import React from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "signup",
+    element: <SignUpPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "home",
+    element: <UserHome />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "pantry",
+    element: <UserPantryPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
